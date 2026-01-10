bc96c1fa004e875a64dcf18c0aca5565b364c92d
ihortok <itokaryk@gmail.com>
2026-01-10 01:00:55 +0100

    Add forms and views for chapters, lessons, subjects, and words with error handling and styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def pluralize(count, singular, few, many)
+    case count % 100
+    when 11..14
+      "#{count} #{many}"
+    else
+      case count % 10
+      when 1
+        "#{count} #{singular}"
+      when 2..4
+        "#{count} #{few}"
+      else
+        "#{count} #{many}"
+      end
+    end
+  end
 end
